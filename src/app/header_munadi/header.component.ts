eb79abd8a4bdb4e44440ad25b07f8cac2f3e851d
kira995 <tranductuan1995@gmail.com>
2019-04-10 18:53:39 +0700

    header

---
+++ src/app/header_munadi/header.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({ 
+    templateUrl: '../header_munadi/header.component.html',
+    selector: 'rose-word',
+    styleUrls: ['../header_munadi/header.component.css']
+})
+export class wordcomponent {}
--- src/app/structure_munadi/word.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-@Component({ 
-    templateUrl: '../structure_munadi/word.component.html',
-    selector: 'rose-word',
-    styleUrls: ['../structure_munadi/word.component.css']
-})
-export class wordcomponent {}
